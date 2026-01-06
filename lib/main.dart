e5dd0d497de228f000abb5b685cb0938668f5844
Vinaya Augusthy <vinayaaugusthy@gmail.com>
2026-01-06 16:39:52 +0530

    chore: initial project setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_app/firebase_options.dart';
 import 'package:notes_app/presentation/auth/login_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
